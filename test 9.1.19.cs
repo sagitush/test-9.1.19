81d39f97a0d22ee57899834f055d3bbce280ddc3
sagitush <44786027+sagitush@users.noreply.github.com>
2019-01-09 21:08:28 +0200

    Add files via upload

---
+++ test 9.1.19.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sheela_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//it was changed because its the same array that I was sending to the function.
+			int[] a = { 5, 4, -5 };
+			update(a);
+			Console.WriteLine(a[0]);
+
+		}
+
+		private static void update(int [] a)
+		{
+			a[0] = 1;
+		}
+	}
+}
